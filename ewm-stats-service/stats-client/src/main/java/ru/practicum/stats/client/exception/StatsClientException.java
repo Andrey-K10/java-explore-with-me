9eb0d5580ccb1140d03c1509d05b921e2821d1a6
Andre <andrey-k10@yandex.ru>
2026-04-16 22:24:22 +0400

    Этап 2. Основной сервис

--- ewm-stats-service/stats-client/src/main/java/ru/practicum/stats/client/exception/StatsClientException.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.stats.client.exception;
-
-public class StatsClientException extends RuntimeException {
-    public StatsClientException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
--- ewm-stats-service/stats-server/src/main/java/ru/practicum/stats/server/exception/ConditionsNotMetException.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.stats.server.exception;
-
-public class ConditionsNotMetException extends RuntimeException {
-    public ConditionsNotMetException(String message) {
-        super(message);
-    }
-}
